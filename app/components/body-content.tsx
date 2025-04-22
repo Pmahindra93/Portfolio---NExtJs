34a739a4c7eeb82b072e937618c29f6c95089975
Prateek Mahindra <prateekmahindra@gmail.com>
2025-04-22 16:29:05 +0100

    added a footer

--- app/components/body-content.tsx
+++ app/components/body-content.tsx
@@ -24,10 +24,16 @@ export function BodyContent({
           <NavBar className="z-40" />
           <div className="flex-1 flex">
             <Sidebar className="hidden md:block z-30" />
-            <main className="flex-1 md:pl-[200px] px-4 pt-16 theme-90s:bg-[#C0C0C0]">
+            <main className="flex-1 md:pl-[200px] px-4 pt-16 pb-16 theme-90s:bg-[#C0C0C0]">
               {children}
             </main>
           </div>
+          {/* Add Footer Here */}
+          <footer className="py-4 border-t border-slate-200 dark:border-slate-800">
+            <p className="text-center text-sm text-muted-foreground">
+              Made with ❤️ in North London with 🤖 and 🍵
+            </p>
+          </footer>
         </div>
       </Providers>
     </body>
